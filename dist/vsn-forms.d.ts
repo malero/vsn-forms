0468e0b8d3d99436e973aa932dcbafcf5c34aa43
malero <contact@maleero.com>
2022-04-28 08:12:50 -0500

    Include form controller in exports

--- dist/vsn-forms.d.ts
+++ dist/vsn-forms.d.ts
@@ -1,2 +1,3 @@
 export { FormAttribute } from './attributes/FormAttribute';
 export { FormControlAttribute } from './attributes/FormControlAttribute';
+export { Form } from './Form';
--- dist/vsn-forms.js
+++ dist/vsn-forms.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.FormControlAttribute = exports.FormAttribute = void 0;
+exports.Form = exports.FormControlAttribute = exports.FormAttribute = void 0;
 var FormAttribute_1 = require("./attributes/FormAttribute");
 Object.defineProperty(exports, "FormAttribute", { enumerable: true, get: function () { return FormAttribute_1.FormAttribute; } });
 var FormControlAttribute_1 = require("./attributes/FormControlAttribute");
 Object.defineProperty(exports, "FormControlAttribute", { enumerable: true, get: function () { return FormControlAttribute_1.FormControlAttribute; } });
+var Form_1 = require("./Form");
+Object.defineProperty(exports, "Form", { enumerable: true, get: function () { return Form_1.Form; } });
 //# sourceMappingURL=vsn-forms.js.map
